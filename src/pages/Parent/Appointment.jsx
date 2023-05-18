b6b3eaf08108270f0b54c255eeaeca8615e86406
Your Name <you@example.com>
2023-05-18 14:44:22 +0200

    change

--- src/pages/Parent/Appointment.jsx
+++ src/pages/Parent/Appointment.jsx
@@ -31,14 +31,14 @@ const Appointment = () => {
         <div>
             <Header />
             <div className="login h-full py-4 font-normal ">
-                <div className="forg flex justify-center  mx-auto h-5/6 p-4 w-4/12 rounded-2xl mt-12" >
+                <div className="forg flex justify-center  mx-auto h-5/6 p-4 w-4/12 rounded-2xl mt-7" >
                     <form className=' p-12 flex  flex-col ' onSubmit={handleSignup}>
 
                         <p className='font-bold text-2xl'>MAKE YOUR APPOINTMENT</p>
-                        <label className='mt-16 font-bold text-xl'>Date</label>
-                        <input type='text' className='p-2 w-full mt-4 rounded-3xl' placeholder='2023-05-18' onChange={handleInputChange}/>
+                        <label className='mt-9 font-bold text-xl'>Date</label>
+                        <input type='text' name="appointment_date" className='p-2 w-full mt-4 rounded-3xl' placeholder='2023-05-18' onChange={handleInputChange}/>
                         <label className='mt-4 font-bold text-xl'>Reason</label>
-                        <textarea type='text' className='px-7 pb-44 w-full mt-4 rounded-3xl' onChange={handleInputChange}/>
+                        <textarea type='text' name="reason" className='px-7 pb-44 w-full mt-4 rounded-3xl' onChange={handleInputChange}/>
                         <button type="submit" className='font-bold mt-5 rounded-xl p-2 dropbtn mx-20'>Send Appointment</button>
                     </form>
                 </div>
